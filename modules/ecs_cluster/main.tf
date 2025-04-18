53fbdb27f9c1a823ca91901e0cda1b61fafafade
Pallavi-1104 <tarkeshubham2006@gmail.com>
2025-04-18 13:52:14 +0530

    qwert

--- modules/ecs_cluster/main.tf
+++ modules/ecs_cluster/main.tf
@@ -2,3 +2,46 @@ resource "aws_ecs_cluster" "cluster" {
   name = var.cluster_name
 }
 
+# Create EC2 Instance for ECS Cluster
+resource "aws_instance" "ecs_instance" {
+  ami           = "ami-0c94855ba95c71c99"
+  instance_type = "t2.micro"
+  iam_instance_profile = aws_iam_instance_profile.ecs_instance_profile.name
+}
+
+# Create IAM Role for ECS Instance
+resource "aws_iam_role" "ecs_instance_role" {
+  name        = "ecs-instance-role"
+  description = "Role for ECS instance"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Effect = "Allow"
+      }
+    ]
+  })
+}
+
+# Create IAM Instance Profile for ECS Instance
+resource "aws_iam_instance_profile" "ecs_instance_profile" {
+  name = "ecs-instance-profile"
+  role = aws_iam_role.ecs_instance_role.name
+}
+
+# Create Subnet
+resource "aws_subnet" "example" {
+  cidr_block = "10.0.1.0/24"
+  vpc_id     = aws_vpc.example.id
+  availability_zone = "us-west-2a"
+}
+
+# Create VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+}
--- modules/ecs_services/main.tf
+++ modules/ecs_services/main.tf
@@ -13,3 +13,23 @@ resource "aws_ecs_service" "mongodb" {
   launch_type      = var.mongodb_service.launch_type
   desired_count   = var.mongodb_service.desired_count
 }
+
+# Create Security Group for ECS Instances
+resource "aws_security_group" "ecs_instances" {
+  name        = "ecs-instances-sg"
+  description = "Allow inbound traffic for ECS instances"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
