88753e23617147af68f47dee95fc9cd6acea3ab6
Pallavi-1104 <tarkeshubham2006@gmail.com>
2025-04-22 10:38:04 +0530

    Update main.tf

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -1,1 +1,34 @@
+resource "aws_lb" "ecs_alb" {
+  name               = "ecs-monitoring-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [var.alb_sg_id]
+  subnets            = var.public_subnet_ids
+}
 
+resource "aws_lb_target_group" "prometheus" {
+  name        = "prometheus-tg"
+  port        = 9090
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
+  target_type = "instance"
+}
+
+resource "aws_lb_target_group" "grafana" {
+  name        = "grafana-tg"
+  port        = 3000
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
+  target_type = "instance"
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.ecs_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.prometheus.arn
+  }
+}
