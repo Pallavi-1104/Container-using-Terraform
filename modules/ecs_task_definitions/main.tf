fdd558f22ef941927007dda82f050bd7167ad5cd
Pallavi-1104 <tarkeshubham2006@gmail.com>
2025-04-22 10:33:06 +0530

    Update main.tf

--- modules/ecs_task_definitions/main.tf
+++ modules/ecs_task_definitions/main.tf
@@ -1,16 +1,23 @@
-resource "aws_ecs_task_definition" "nodejs" {
-  family                = var.nodejs_task_definition.family
-  cpu                    = var.nodejs_task_definition.cpu
-  memory                = var.nodejs_task_definition.memory
-  network_mode          = var.nodejs_task_definition.network_mode
-  container_definitions = var.nodejs_task_definition.container_definitions
-}
-
 resource "aws_ecs_task_definition" "mongodb" {
-  family                = var.mongodb_task_definition.family
-  cpu                    = var.mongodb_task_definition.cpu
-  memory                = var.mongodb_task_definition.memory
-  network_mode          = var.mongodb_task_definition.network_mode
-  container_definitions = var.mongodb_task_definition.container_definitions
+  family                   = var.mongodb_task_definition.family
+  cpu                      = var.mongodb_task_definition.cpu
+  memory                   = var.mongodb_task_definition.memory
+  network_mode             = var.mongodb_task_definition.network_mode
+  requires_compatibilities = ["EC2"]
+  container_definitions    = jsonencode(var.mongodb_task_definition.container_definitions)
+
+ volume {
+    name = "mongo-volume"
+    host_path = "/ecs/mongo-data"
+  }
 }
 
+
+resource "aws_ecs_task_definition" "nodejs" {
+  family                   = var.nodejs_task_definition.family
+  cpu                      = var.nodejs_task_definition.cpu
+  memory                   = var.nodejs_task_definition.memory
+  network_mode             = var.nodejs_task_definition.network_mode
+  requires_compatibilities = ["EC2"]
+  container_definitions    = jsonencode(var.nodejs_task_definition.container_definitions)
+}
