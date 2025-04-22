86f4de5b7a761c4bf0559181847fde2bd5bfa099
Pallavi-1104 <tarkeshubham2006@gmail.com>
2025-04-22 10:39:15 +0530

    Update outputs.tf

--- modules/alb/outputs.tf
+++ modules/alb/outputs.tf
@@ -1,1 +1,11 @@
+output "prometheus_tg_arn" {
+  value = aws_lb_target_group.prometheus.arn
+}
 
+output "grafana_tg_arn" {
+  value = aws_lb_target_group.grafana.arn
+}
+
+output "alb_dns_name" {
+  value = aws_lb.ecs_alb.dns_name
+}
