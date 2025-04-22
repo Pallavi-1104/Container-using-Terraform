f2ed7e9d623f7cac63afea438283346e18f66548
Pallavi-1104 <tarkeshubham2006@gmail.com>
2025-04-22 10:25:20 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,56 @@
 variable "aws_region" {
   type        = string
-  default     = "us-west-2"
+  default     = "us-east-1"
   description = "AWS region to deploy to"
+}
+
+
+# Network Variables
+
+variable "vpc_cidr_block" {
+  type = string
+}
+
+variable "public_subnet_cidrs" {
+  type = list(string)
+}
+
+variable "availability_zones" {
+  type = list(string)
+}
+
+
+# ECS Cluster / EC2
+
+variable "key_name" {
+  type = string
+}
+
+variable "cluster_name" {
+  type = string
+}
+
+
+# Task Definitions for ECS
+
+variable "prometheus_task_definition" {
+  type = any
+}
+
+variable "grafana_task_definition" {
+  type = any
+}
+
+variable "nodejs_task_definition" {
+  type = any
+}
+
+variable "mongodb_task_definition" {
+  type = any
+}
+
+variable "environment" {
+  description = "Deployment environment name (e.g. dev, staging, prod)"
+  type        = string
+  default     = "dev"
 }
